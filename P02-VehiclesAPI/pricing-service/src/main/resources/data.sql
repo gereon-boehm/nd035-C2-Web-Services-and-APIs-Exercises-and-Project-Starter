7dd074e998af033637f8134993a49568ba484907
Gereon Böhm <gereon.boehm@ibeo-as.com>
2021-08-31 09:23:52 +0200

    Convert pricing service api into microservice

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('EUR', 1000, 1);
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('EUR', 2000, 2);
+INSERT INTO Price (currency, price, vehicle_id) VALUES ('EUR', 3000, 3);
