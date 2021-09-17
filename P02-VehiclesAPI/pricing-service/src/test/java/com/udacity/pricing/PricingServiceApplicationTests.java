641815034421fc9b04c8f4d4dd8503d363c50392
Gereon Böhm <gereon.boehm@ibeo-as.com>
2021-09-17 08:43:30 +0200

    Add test to pricing service

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,51 @@
 package com.udacity.pricing;
 
+import com.udacity.pricing.domain.price.Price;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
+
+	@LocalServerPort
+	int port;
+
+	@Autowired
+	MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void get_price_by_id() throws Exception{
+		final String link = "http://localhost:" + port + "/prices/1";
+		mockMvc.perform(MockMvcRequestBuilders
+				.get(link)
+				.accept(MediaType.APPLICATION_JSON_UTF8))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.currency").exists())
+				.andExpect(jsonPath("$.currency").value("EUR"))
+				.andExpect(jsonPath("$.price").exists())
+				.andExpect(jsonPath("$.price").value("1000.0"))
+		;
+	}
 }
